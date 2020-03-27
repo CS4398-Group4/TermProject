a3b62c713b64a167fa52055f90a016e2d55da20c
Travis CI <builds@travis-ci.com>
2020-03-26 19:19:50 -0500

    m

--- cogs/mlb.py
+++ cogs/mlb.py
@@ -6,48 +6,41 @@ from discord.ext import commands
 
 
 class MLB(commands.Cog):
-
     def __init__(self, bot):
-        self.__name__ = "MLB Extension"
         self.bot = bot
-        self.schedule_array = []
-        self.today = datetime.now().date()
+        self.__name__ = "MLB Schedule"
+        self.schedule = []
+        self.today = datetime.now().date
 
-    # * Listeners
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.load_schedule()
-        print(f'loaded cog: {self.__name__}')
-
-    # * Commands
-    @commands.command(aliases=['mlb'], brief="See The MLB Schedule for Today")
-    async def schedule(self, ctx):
-        today_str = self.today.strftime("%B %-d, %Y")
-        today_compare = self.today.strftime("%Y-%m-%d")
-        todays_games = []
-
-        for game in self.schedule_array:
-            if game['date'] == today_compare:
-                todays_games.append(game)
-
-        embed = discord.Embed(
-            title=f"MLB Games for {today_str}")
-
-        count = 1
-        for game in todays_games:
-            embed.add_field(name=f"Game {count}",
-                            value=f"{game['visitor_name']} vs {game['home_name']} @ {game['gametimeET']} ET")
-            count += 1
-
-        await ctx.send(embed=embed)
-
-    #  * Helpers
-    def load_schedule(self):
+    def load_mlb_schedule(self):
         with open(os.path.join(os.path.dirname(__file__), 'assets/mlb_schedule.json')) as infile:
             data = json.load(infile)
+        for game in data:
+            self.schedule.append(game)
 
-            for game in data:
-                self.schedule_array.append(game)
+    # listener
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.load_mlb_schedule()
+        print(f'loaded cog: {self.__name__}')
+
+    # command
+    @commands.command(aliases=['mlb'], brief="See The MLB Schedule for today")
+    async def get_schedule(self, ctx):
+        count = 1
+        current1 = self.today.strftime("%B %-d, %Y")
+        current = self.today.strftime("%Y-%m-%d")
+        current_games = []
+
+        for games in self.schedule:
+            if games['date'] == current:
+                current_games.append(games)
+        embed = discord.Embed(title=f"MLB Games for {current1}")
+
+        for games in current_games:
+            embed.add_field(name=f"Game {count}", value=f"{games['Visitor']} vs {games['Home']} @ {games['GameTimeET']} ET")
+            count += 1
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
